3cb7dd801d772a0647eb89c06f97d13b1712c1f7
Alejandro Migues <alejandro.migues@outlook.com>
2024-08-06 10:56:52 -0300

    Added Tailwind

--- projects/frontend/app/src/App.tsx
+++ projects/frontend/app/src/App.tsx
@@ -1,3 +1,7 @@
 export function App() {
-  return <h1>Kanban</h1>;
+  return (
+    <div className='w-full min-h-screen flex flex-col justify-center items-center'>
+      <h1 className='text-4xl font-bold underline'>Kanban</h1>
+    </div>
+  );
 }
