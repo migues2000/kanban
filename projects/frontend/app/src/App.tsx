5cde4b63979c7b37c14378026b44b47881f0c17b
Alejandro Migues <alejandro.migues@outlook.com>
2024-08-06 11:16:24 -0300

    Added Shadcn

--- projects/frontend/app/src/App.tsx
+++ projects/frontend/app/src/App.tsx
@@ -1,7 +1,20 @@
+import { Button } from './components/ui/button';
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from './components/ui/card';
+
 export function App() {
   return (
     <div className='w-full min-h-screen flex flex-col justify-center items-center'>
-      <h1 className='text-4xl font-bold underline'>Kanban</h1>
+      <Card className='w-80'>
+        <CardHeader>
+          <CardTitle>Kanban</CardTitle>
+          <CardDescription>The best Kanban app</CardDescription>
+        </CardHeader>
+
+        <CardFooter className='flex flex-row justify-between'>
+          <Button variant='outline'>Hello World!</Button>
+          <Button>Hello World!</Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
--- projects/frontend/app/vite.config.ts
+++ projects/frontend/app/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
 import { default as react } from '@vitejs/plugin-react-swc';
 
+import { default as path } from 'path';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
